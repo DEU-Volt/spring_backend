8e4c53c9bd012966cb07b0490027d7fb0eb3d2b6
K-moovie <kyha97@naver.com>
2021-03-27 18:17:57 +0900

    Add Spring Actuator & Spring Boot Admin

---
+++ microservices/product-service/src/test/java/deu/se/volt/microservices/core/product/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package deu.se.volt.microservices.core.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
