803f2d2a064c4c0e1a904e4e7d1872158961f5b6
K-moovie <kyha97@naver.com>
2021-04-11 23:32:05 +0900

    Add Eureka Replication

--- eureka-server/src/main/java/deu/se/volt/eurekaserver_replication/EurekaServerApplication.java
+++ eureka-server/src/main/java/deu/se/volt/eurekaserver_replication/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package deu.se.volt.eurekaserver;
+package deu.se.volt.eurekaserver_replication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- eureka-server/src/test/java/deu/se/volt/eurekaserver_replication/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/deu/se/volt/eurekaserver_replication/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package deu.se.volt.eurekaserver;
+package deu.se.volt.eurekaserver_replication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
