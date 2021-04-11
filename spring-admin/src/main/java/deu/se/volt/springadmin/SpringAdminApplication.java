2dfc91c4d3af7efb166cdc3967d9b9b41d0a3e19
SWKANG0525 <kanghodong@kakao.com>
2021-04-11 21:43:57 +0900

    Add Eureka Server & Client

--- spring-admin/src/main/java/deu/se/volt/springadmin/SpringAdminApplication.java
+++ spring-admin/src/main/java/deu/se/volt/springadmin/SpringAdminApplication.java
@@ -4,8 +4,9 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 @EnableAdminServer
+@SpringBootApplication
 public class SpringAdminApplication {
 
     public static void main(String[] args) {
