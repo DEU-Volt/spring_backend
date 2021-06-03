b801b9ac1aca93ab7623abe0276711222aa9d181
SWKANG0525 <kanghodong@kakao.com>
2021-06-04 07:35:42 +0900

    Add Product Entity & getProduct API

--- microservices/product-service/src/main/java/deu/se/volt/microservices/core/product/ProductServiceApplication.java
+++ microservices/product-service/src/main/java/deu/se/volt/microservices/core/product/ProductServiceApplication.java
@@ -3,6 +3,7 @@ package deu.se.volt.microservices.core.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableEurekaClient
 @SpringBootApplication
--- microservices/product-service/src/main/java/deu/se/volt/microservices/core/product/controller/ProductController.java
+++ microservices/product-service/src/main/java/deu/se/volt/microservices/core/product/controller/ProductController.java
@@ -1,22 +1,51 @@
 package deu.se.volt.microservices.core.product.controller;
 
+import deu.se.volt.authorizationserver.util.StatusCode;
+import deu.se.volt.microservices.core.product.entity.Product;
+import deu.se.volt.microservices.core.product.service.ProductService;
+import deu.se.volt.microservices.core.product.util.DefaultResponse;
+import deu.se.volt.microservices.core.product.util.ResponseMessage;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @RestController
-@Api
+@Api("상품 컨트롤러 V1")
+@AllArgsConstructor
 public class ProductController {
+    private final ProductService productService;
+
     @ApiOperation(value = "test", notes = "테스트입니다.")
-    @GetMapping("/product/test")
-    public String testFunc() {
-        return "test called";
+    @GetMapping("/product")
+    public ResponseEntity getProduct(@RequestParam @Valid final String productName) {
+
+        try {
+            Map<String, Product> map = new HashMap<>();
+            var product = productService.loadProductByProductName(productName);
+            map.put("result", product);
+
+            return new ResponseEntity(DefaultResponse.res(
+                    StatusCode.OK,
+                    ResponseMessage.PRODUCT_SUCCESS,
+                    map), HttpStatus.OK);
+
+        } catch (NoSuchElementException elementException) {
+            Map<String, String> map = new HashMap<>();
+            map.put("result","false");
+
+            return new ResponseEntity(DefaultResponse.res(
+                    StatusCode.NOT_FOUND,
+                    ResponseMessage.NOT_FOUND_PRODUCT,
+                    map), HttpStatus.OK);
+        }
+
     }
 }
