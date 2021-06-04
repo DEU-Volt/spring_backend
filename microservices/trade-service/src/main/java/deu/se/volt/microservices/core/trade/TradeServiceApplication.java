b1fb4ddf0d399c12172bc629401ebb6c1ea1ed4a
K-moovie <kyha97@naver.com>
2021-06-04 22:06:22 +0900

    Add Trade-service module

---
+++ microservices/trade-service/src/main/java/deu/se/volt/microservices/core/trade/TradeServiceApplication.java
@@ -0,0 +1,13 @@
+package deu.se.volt.microservices.core.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeServiceApplication.class, args);
+	}
+
+}
